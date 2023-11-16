64d7bea885ced40e3361eddf84a8c12e49451cbe
Assekour-Mohamed <72951139+Assekour-Mohamed@users.noreply.github.com>
2023-11-16 22:22:22 +0100

    Create Car1Class.py

---
+++ Car1Class.py
@@ -0,0 +1,24 @@
+class Car:
+  def __init__( self, speed,enginePower, color, name):
+    {
+      self.speed = speed 
+      self.enginePower = engiePower
+      self.color = color
+      self.name = name
+    }
+  def start():
+  {
+    #starting  code
+  }
+  def stop():
+    {
+      #stoping code
+    }
+  def changGear():
+    {
+      #Changing code
+    }
+ 
+bmw = car(23, 111, "yallow","bmw 1333")
+bmw.start()
+bmw.stop()
