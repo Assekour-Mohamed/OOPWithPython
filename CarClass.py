5b23d92a8606d10629abe791c1146aae05054e18
Assekour-Mohamed <72951139+Assekour-Mohamed@users.noreply.github.com>
2023-11-16 22:19:52 +0100

    Create CarClass.py

---
+++ CarClass.py
@@ -0,0 +1,26 @@
+class Car:
+  def __init__( self, speed, color, nitroSpeed, model):
+    {
+      self.speed = speed 
+      self.color = color
+      self.nitroSpeed = model
+    }
+  def turn():
+  {
+    #turning  code
+  }
+  def accelerate():
+    {
+      #accelerating code
+    }
+  def brake():
+    {
+      #braking code
+    }
+  def boost():
+    {
+      #boodting code
+    }
+bmw = car(23, 111, true,"bmw 1333")
+bmw.turn()
+bmw.brake()
