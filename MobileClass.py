908d5b0ece5a620f4877b07dc75ee346408e1a3b
Assekour-Mohamed <72951139+Assekour-Mohamed@users.noreply.github.com>
2023-11-16 22:32:21 +0100

    Create MobileClass.py

---
+++ MobileClass.py
@@ -0,0 +1,26 @@
+class Mobile:
+  def __init__(self,companyName, storage, serialNum, name, dualSim, support4k)
+    {
+      self.companyNmae = companyName
+      self.storage = storage
+      self.serialNum = serialNum
+      self.name = name
+      self.dualSim = dualSim
+      self.support4k = support4k
+    }
+  def call():
+    {
+      #calling code
+    }
+  def sendMessage():
+    {
+      #sending messages code
+    }
+  def playMedia():
+    {
+      #plating media
+    }
+samsung = Mobile("samsung", 256 , 33F4 ,"Samsung",true,true)
+samsung.call()
+
+
