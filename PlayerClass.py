2b425f7cf6226ac58ffe4937484459c2c0dc86fb
Assekour-Mohamed <72951139+Assekour-Mohamed@users.noreply.github.com>
2023-11-16 22:14:42 +0100

    Create PlayerClass.py

---
+++ PlayerClass.py
@@ -0,0 +1,23 @@
+class Player:
+  def __init__( self, name, age, rank):
+    {
+      self.name = name
+      self.age = age
+      self.rank = rank
+    }
+  def pass():
+  {
+    #passing ball code
+  }
+  def shoot():
+    {
+      #shooting bass code
+    }
+  def jump():
+    {
+      #jumping code
+    }
+halland = Palyer("halland", 23, 44)
+halland.pass()
+halland.shoot()
+halland.jump()
